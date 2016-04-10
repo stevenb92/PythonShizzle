69a70d78c289e00c11b360971c8e745f42285a05
Steven Byrne <steveby@cyberduckv2.home>
2016-04-10 21:57:01 +0100

    tipcalc.py

---
+++ tipcalc.py
@@ -0,0 +1,14 @@
+# Tip counter to work out the total of a given bill after
+# adding a tip of a given percentage.
+
+#Initialize the variables
+bill = 0
+tipPerc = 0
+total= 0
+
+print ("How much was the bill?:")
+bill = input() 
+print ("How much would you like to tip? (In Percent %):")
+tipPerc = input ()
+total = bill 
+
